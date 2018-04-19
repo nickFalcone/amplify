36f7066c1bfd948bdd4ef773be966ffd9344c3f3
nickFalcone <nfalcone@u.rochester.edu>
2018-04-19 19:38:10 -0400

    set up initial project files for v1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const htmlmin = require('gulp-htmlmin');
+const gulpAmpValidator = require('gulp-amphtml-validator');
+const plumber = require('gulp-plumber');
+const cssnano = require('gulp-cssnano');
+const image = require('gulp-image');
+const fs = require("fs");
+const inject = require('gulp-inject-string');
+const browser = require('browser-sync');
+const reload = browser.reload;
+
+gulp.task('scss', () => {
+  gulp.src('src/style/style.scss')
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(cssnano())
+      .pipe(gulp.dest('src/style'));
+});
+
+gulp.task('html', () => {
+    let cssContent = fs.readFileSync("src/style/style.css", "utf8");
+    gulp.src("src/components/index.html")
+        .pipe(inject.after('style amp-custom>', cssContent))
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest("public/"))
+        .pipe(reload({
+            stream: true
+    }));
+});
+
+gulp.task('amphtml:validate', () => {
+  return gulp.src("public/index.html")
+    .pipe(gulpAmpValidator.validate())
+    .pipe(gulpAmpValidator.format())
+    .pipe(gulpAmpValidator.failAfterError());
+});
+
+gulp.task('serve', () => {
+    browser({
+        port: 3000,
+        open: false,
+        ghostMode: false,
+        server: {
+            baseDir: 'public'
+        }
+    });
+});
+
+gulp.task('images', () => {
+  return gulp.src('src/img/*')
+    .pipe(image({
+        pngquant: true,
+        optipng: true,
+        zopflipng: true,
+        jpegRecompress: true,
+        mozjpeg: true,
+        guetzli: false,
+        gifsicle: true,
+        svgo: true,
+        concurrent: 10,
+        quiet: false
+    }))
+    .pipe(gulp.dest('public/img/'))
+});
+
+gulp.task('watch', () => {
+  gulp.watch("src/style/style.scss", ['scss']);
+  gulp.watch("src/style/style.css", ['html']);
+  gulp.watch("src/components/*.html", ['html']);
+  gulp.watch("src/img/**", ['images']);
+  gulp.watch("public/index.html", ['amphtml:validate'])
+});
+
+gulp.task('default', ['scss', 'html', 'images', 'amphtml:validate', 'watch', 'serve']);
+
