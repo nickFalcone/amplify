1331aeee7f533d09feb1195fbc8e62000f8326ea
nickFalcone <nfalcone@u.rochester.edu>
2018-11-04 14:59:55 -0500

    add amp-bind-macro example; prevent background from scrolling when lightbox is active

--- gulpfile.js
+++ gulpfile.js
@@ -14,28 +14,28 @@ const reload = browser.reload;
 
 gulp.task('scss', () => {
   gulp.src('src/style/style.scss')
-      .pipe(plumber())
-      .pipe(sass())
-      .pipe(cssnano())
-      .pipe(gulp.dest('src/style'));
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(gulp.dest('src/style'));
 });
 
 gulp.task('html', () => {
-    let cssContent = fs.readFileSync("src/style/style.css", "utf8");
-    gulp.src("src/components/*.html")
-        .pipe(inject.after('style amp-custom>', cssContent))
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest("public/"))
-        .pipe(reload({
-            stream: true
+  let cssContent = fs.readFileSync("src/style/style.css", "utf8");
+  gulp.src("src/components/*.html")
+    .pipe(inject.after('style amp-custom>', cssContent))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("public/"))
+    .pipe(reload({
+      stream: true
     }));
 });
 
 gulp.task('data', () => {
-    gulp.src("src/data/data.json")
-        .pipe(gulp.dest("public/data/"))
-        .pipe(reload({
-            stream: true
+  gulp.src("src/data/*.json")
+    .pipe(gulp.dest("public/data/"))
+    .pipe(reload({
+      stream: true
     }));
 });
 
@@ -47,46 +47,46 @@ gulp.task('amphtml:validate', () => {
 });
 
 gulp.task('serve', () => {
-    browser({
-        port: 3000,
-        open: false,
-        ghostMode: false,
-        server: {
-            baseDir: 'public'
-        }
-    });
+  browser({
+    port: 3000,
+    open: false,
+    ghostMode: false,
+    server: {
+      baseDir: 'public'
+    }
+  });
 });
 
 gulp.task('images', () => {
   return gulp.src('src/img/*')
     .pipe(image({
-        pngquant: true,
-        optipng: true,
-        zopflipng: true,
-        jpegRecompress: true,
-        mozjpeg: true,
-        guetzli: false,
-        gifsicle: true,
-        svgo: true,
-        concurrent: 10,
-        quiet: false
+      pngquant: true,
+      optipng: true,
+      zopflipng: true,
+      jpegRecompress: true,
+      mozjpeg: true,
+      guetzli: false,
+      gifsicle: true,
+      svgo: true,
+      concurrent: 10,
+      quiet: false
     }))
     .pipe(gulp.dest('public/img/'))
 });
 
 // clean task to keep public directory in sync when files are removed from /src
 gulp.task('clean', () => {
-    return del('public/**', {force:true});
+  return del('public/**', { force: true });
 });
 
 // clean needs to run first. then build html/css and compress images.  then validate. then serve, and finally watch task.
 gulp.task('default', gulpSequence('clean', ['html', 'scss', 'images', 'data'], 'amphtml:validate', 'serve', 'watch'));
 
 gulp.task('watch', () => {
-    gulp.watch("src/style/style.scss", ['scss']);
-    gulp.watch("src/style/style.css", ['html']);
-    gulp.watch("src/components/*.html", ['html']);
-    gulp.watch("src/data/data.json", ['data']);
-    gulp.watch("src/img/**", ['images']);
-    gulp.watch("public/*.html", ['amphtml:validate'])
-  });
+  gulp.watch("src/style/style.scss", ['scss']);
+  gulp.watch("src/style/style.css", ['html']);
+  gulp.watch("src/components/*.html", ['html']);
+  gulp.watch("src/data/data.json", ['data']);
+  gulp.watch("src/img/**", ['images']);
+  gulp.watch("public/*.html", ['amphtml:validate'])
+});
